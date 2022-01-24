37363c74223729ed73753a0f7d8a73cd2b5429ca
yui <yui@1xp.me>
2022-01-23 22:21:33 -0800

    reorganize the project

---
+++ MDT.Core/Manager/CardMgr.cs
@@ -0,0 +1,23 @@
+﻿using MDT.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MDT.Core.Manager
+{
+    public class CardMgr:BaseManager<CardMgr>
+    {
+        private Dictionary<string, CardInfo> _database;
+        public CardMgr()
+        {
+            string cardStr = File.ReadAllText("cards.json");
+            _database = JsonConvert.DeserializeObject<Dictionary<string, CardInfo>>(cardStr);
+        }
+        public CardInfo GetCardInfo(string id)
+        {
+            if (_database.ContainsKey(id))
+                return _database[id];
+            return null;
+        }
+    }
+}
